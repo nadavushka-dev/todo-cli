f11c88b8832c2f23e36127483e283233025f861b
nadavushka-dev <nadavushkadev@gmail.com>
2025-03-09 10:33:54 +0200

    main application with flags

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	"todo.com/m/notes"
+)
+
+func main() {
+	add := flag.String("a", "", "Add a new todo")
+	ls := flag.Bool("ls", false, "List all todos")
+	done := flag.Int("d", 0, "Mark selected task as done")
+	clear := flag.Bool("c", false, "Clear all todos")
+
+	flag.Parse()
+
+	reader := bufio.NewReader(os.Stdin)
+	file := notes.ReadTodosFile()
+
+	if *clear {
+		HandleClearFlag(file)
+		return
+	}
+
+	if *done > 0 {
+		HandleDoneFlag(file, *done)
+		return
+	}
+
+	if *add != "" {
+		if flag.NArg() > 0 {
+			*add += " " + strings.Join(flag.Args(), " ")
+		}
+		HandleAddFlag(reader, file, *add)
+		return
+	}
+
+	if *ls {
+		HandleListFlag(file)
+		return
+	}
+
+	fmt.Println("Usage: ")
+	flag.PrintDefaults()
+
+	defer file.Close()
+}
