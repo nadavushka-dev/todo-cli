b67eb90b186b2ffef6d4eddaf6bb1af191e103b0
nadavushka-dev <nadavushkadev@gmail.com>
2025-03-11 10:27:56 +0200

    added no flag run functionality

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 
 	"todo.com/m/notes"
@@ -15,10 +16,17 @@ func main() {
 	ls := flag.Bool("ls", false, "List all todos")
 	done := flag.Int("d", 0, "Mark selected task as done")
 	clear := flag.Bool("c", false, "Clear all todos")
+	help := flag.Bool("h", false, "help command")
+
+	reader := bufio.NewReader(os.Stdin)
 
 	flag.Parse()
 
-	reader := bufio.NewReader(os.Stdin)
+	if flag.NFlag() == 0 {
+		RunNoFlags(reader)
+		return
+	}
+
 	file := notes.ReadTodosFile()
 	defer file.Close()
 
@@ -45,7 +53,45 @@ func main() {
 		return
 	}
 
-	fmt.Println("Usage: ")
-	flag.PrintDefaults()
+	if *help {
+		fmt.Println("Usage: ")
+		flag.PrintDefaults()
+	} else {
+	}
+}
 
+func RunNoFlags(reader *bufio.Reader) {
+	var run bool = true
+	for run {
+		file := notes.ReadTodosFile()
+
+		fmt.Println("select option: ")
+		fmt.Println("1: list all todos")
+		fmt.Println("2: add a todo")
+		fmt.Println("3: complete todo")
+		fmt.Println("4: clear todo list")
+		fmt.Println("q: quite program")
+		opt, _ := reader.ReadString('\n')
+		opt = strings.TrimSpace(opt)
+
+		if opt == "1" {
+			HandleListFlag(file)
+		} else if opt == "2" {
+			fmt.Println("provide a title")
+			title, _ := reader.ReadString('\n')
+			title = strings.TrimSpace(title)
+			HandleAddFlag(reader, file, title)
+		} else if opt == "3" {
+			fmt.Println("provide todo's id")
+			id, _ := reader.ReadString('\n')
+			nId, _ := strconv.Atoi(strings.TrimSpace(id))
+			HandleDoneFlag(file, nId)
+		} else if opt == "4" {
+			HandleClearFlag(file)
+		} else if opt == "q" {
+			run = false
+		}
+
+		file.Close()
+	}
 }
