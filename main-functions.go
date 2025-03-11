d89514fc6d7adf5bcddd4fdafd037cabac655f0f
nadavushka-dev <nadavushkadev@gmail.com>
2025-03-11 10:27:27 +0200

    added empty line for better ux

--- main-functions.go
+++ main-functions.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 
 	"todo.com/m/notes"
@@ -12,6 +13,7 @@ func HandleClearFlag(file *os.File) {
 
 	jsonData := notes.ProcessTodosToJsonData(todos)
 	notes.WriteTodosToFile(file, jsonData)
+	fmt.Println()
 }
 
 func HandleAddFlag(reader *bufio.Reader, file *os.File, title string) {
@@ -25,18 +27,23 @@ func HandleAddFlag(reader *bufio.Reader, file *os.File, title string) {
 
 	jsonData := notes.ProcessTodosToJsonData(todos)
 	notes.WriteTodosToFile(file, jsonData)
+	fmt.Println()
 }
 
 func HandleListFlag(file *os.File) {
 	todos := notes.GetTodosData(file)
 	notes.ProcessTodosToJsonData(todos)
+	fmt.Println()
 }
 
 func HandleDoneFlag(file *os.File, id int) {
 	todos := notes.GetTodosData(file)
+	fmt.Println("todos: ", todos)
 
 	for i, todo := range todos {
+		fmt.Println("todo: ", todo, "id: ", id)
 		if todo.Id == id {
+			fmt.Println("here")
 			todo.MarkAsDone()
 			todos[i] = todo
 			break
@@ -45,4 +52,5 @@ func HandleDoneFlag(file *os.File, id int) {
 
 	jsonData := notes.ProcessTodosToJsonData(todos)
 	notes.WriteTodosToFile(file, jsonData)
+	fmt.Println()
 }
